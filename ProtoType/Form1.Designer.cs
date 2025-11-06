f3a70ed7a18b3707bc5266d5a5509de00570307d
Deanlover1 <jdmat690@learner.dvusd.org>
2025-11-06 07:33:14 -0700

    jdm label

--- ProtoType/Form1.Designer.cs
+++ ProtoType/Form1.Designer.cs
@@ -28,11 +28,11 @@ namespace ProtoType
         /// </summary>
         private void InitializeComponent()
         {
-<<<<<<< Updated upstream
             this.ESlabel = new System.Windows.Forms.Label();
             this.MElabel = new System.Windows.Forms.Label();
             this.DMlabel = new System.Windows.Forms.Label();
             this.MGlabel = new System.Windows.Forms.Label();
+            this.jdmlabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // ESlabel
@@ -70,33 +70,26 @@ namespace ProtoType
             this.MGlabel.Size = new System.Drawing.Size(119, 16);
             this.MGlabel.TabIndex = 2;
             this.MGlabel.Text = "Massimo Guagenti";
-=======
-            this.JDMlabel = new System.Windows.Forms.Label();
-            this.SuspendLayout();
             // 
-            // JDMlabel
+            // jdmlabel
             // 
-            this.JDMlabel.AutoSize = true;
-            this.JDMlabel.Location = new System.Drawing.Point(519, 100);
-            this.JDMlabel.Name = "JDMlabel";
-            this.JDMlabel.Size = new System.Drawing.Size(98, 16);
-            this.JDMlabel.TabIndex = 0;
-            this.JDMlabel.Text = "John D. Matzke";
->>>>>>> Stashed changes
+            this.jdmlabel.AutoSize = true;
+            this.jdmlabel.Location = new System.Drawing.Point(654, 25);
+            this.jdmlabel.Name = "jdmlabel";
+            this.jdmlabel.Size = new System.Drawing.Size(93, 16);
+            this.jdmlabel.TabIndex = 3;
+            this.jdmlabel.Text = "John d Matzke";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-<<<<<<< Updated upstream
+            this.Controls.Add(this.jdmlabel);
             this.Controls.Add(this.MGlabel);
             this.Controls.Add(this.DMlabel);
             this.Controls.Add(this.ESlabel);
             this.Controls.Add(this.MElabel);
-=======
-            this.Controls.Add(this.JDMlabel);
->>>>>>> Stashed changes
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -111,6 +104,7 @@ namespace ProtoType
         private System.Windows.Forms.Label MElabel;
         private System.Windows.Forms.Label DMlabel;
         private System.Windows.Forms.Label MGlabel;
+        private System.Windows.Forms.Label jdmlabel;
 =======
         private System.Windows.Forms.Label JDMlabel;
 >>>>>>> Stashed changes
